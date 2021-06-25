b4ccdc4d4bb5a3e5472823d28bcd8f77dc940def
AbraaoM <abraaol.moreira@outlook.com>
2021-06-25 12:36:42 -0300

    Finalizada a geracao de certificado localmente

--- index.js
+++ index.js
@@ -1,49 +1,53 @@
-const { response } = require('express')
+
 const express = require('express')
-const handlebars = require('express-handlebars')
+const fs = require('fs')
+const path = require('path')
+const utils = require('util')
 const puppeteer = require('puppeteer')
+const handlebars = require('express-handlebars')
+const hb = require('handlebars')
+const readFile = utils.promisify(fs.readFile)
+
 const app = express()
 const port = 3000
 
-app.engine('handlebars', handlebars({
-  defaultLayout: 'main',
-}))
+app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.render('template', req.body)
-  
-})
+app.post('/certificado', async (req, res) => {
 
-app.get('/certificado', async (req, res) => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  const template = Handlebars.compile('template');
-  await page.goto('http://localhost:3000/', {
-    waitUntil: 'networkidle0'
-  })
+    const templatePath = path.resolve("./template.html")
+    const templateHTMLString = await readFile(templatePath, "utf8")
 
-  // await page.pdf({ path: 'certificado.pdf', format: 'a4' })
-  
-  const pdf = await page.pdf({
-    printBackground: true,
-    format: 'a4',
-    margin:{
-      top: "20px",
-      bottom: "40px",
-      left: "20px",
-      right: "20px"
-    }
-  })
+    const template = hb.compile(templateHTMLString, { strict: true })
 
-  await browser.close()
+    const templateHTML = template(req.body.data)
 
-  res.contentType("application/pdf")  
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
 
-  return res.send(pdf)
+    await page.setContent(templateHTML)
+
+    const pdf = await page.pdf({
+      path: 'certificado.pdf',
+      printBackground: true,
+      format: 'a4',
+      margin:{
+        top: "20px",
+        bottom: "40px",
+        left: "20px",
+        right: "20px"
+      }
+    })
+
+    await browser.close()
+
+    res.contentType("application/pdf") 
+    
+    return res.send(pdf)
 
 })
 

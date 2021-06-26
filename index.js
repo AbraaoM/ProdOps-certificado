051ba551189767ac14a3cbff2dcc156f74a0a2f0
AbraaoM <abraaol.moreira@outlook.com>
2021-06-26 00:03:09 -0300

    Modificacao puppeteer browser launch

--- index.js
+++ index.js
@@ -30,7 +30,17 @@ app.post('/certificado', async (req, res) => {
 
   const html = template(req.body.data)
 
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+                                          headless: true,
+                                          defaultViewport: null,
+                                          args: [
+                                              "--incognito",
+                                              "--no-sandbox",
+                                              "--single-process",
+                                              "--no-zygote"
+                                          ],
+                                        })
+
   const page = await browser.newPage()
 
   await page.setContent(html)

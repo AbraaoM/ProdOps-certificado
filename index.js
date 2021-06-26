7e5abc6a73f8cd9c75f9e8805e50b08fcb9cd583
AbraaoM <abraaol.moreira@outlook.com>
2021-06-25 21:06:41 -0300

    Geracao de certificado como servico

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const puppeteer = require('puppeteer')
 const handlebars = require('express-handlebars')
 const hb = require('handlebars')
 const readFile = utils.promisify(fs.readFile)
+const axios = require('axios')
 
 const app = express()
-const port = 3000
+const port = 5000
 
 app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
@@ -19,36 +20,38 @@ app.use(express.json())
 
 app.post('/certificado', async (req, res) => {
 
-    const templatePath = path.resolve("./template.html")
-    const templateHTMLString = await readFile(templatePath, "utf8")
+  const templateHTML = await axios({
+                              method: "get",
+                              url: req.body.template,
+                              responseType: "blob",
+                            })                         
 
-    const template = hb.compile(templateHTMLString, { strict: true })
+  const template = hb.compile(templateHTML.data, { strict: true })
 
-    const templateHTML = template(req.body.data)
+  const html = template(req.body.data)
 
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
 
-    await page.setContent(templateHTML)
+  await page.setContent(html)
 
-    const pdf = await page.pdf({
-      path: 'certificado.pdf',
-      printBackground: true,
-      format: 'a4',
-      margin:{
-        top: "20px",
-        bottom: "40px",
-        left: "20px",
-        right: "20px"
-      }
-    })
+  const pdf = await page.pdf({
+    path: 'certificado.pdf',
+    printBackground: true,
+    format: 'a4',
+    margin:{
+      top: "20px",
+      bottom: "40px",
+      left: "20px",
+      right: "20px"
+    }
+  })
 
-    await browser.close()
+  await browser.close()
 
-    res.contentType("application/pdf") 
-    
-    return res.send(pdf)
-
+  res.contentType("application/pdf") 
+  
+  return res.send(pdf)
 })
 
 app.listen(port, () => {
